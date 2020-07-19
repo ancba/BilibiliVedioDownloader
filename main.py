affbe031f646b5bc1f25f760bd9a630c22e9e1a4
ancba <2176678607@qq.com>
2020-07-19 15:37:40 +0800

    update

--- main.py
+++ main.py
@@ -2,19 +2,9 @@ import urllib.request as request
 import api
 import os
 
-data = {
-    "Sec-Fetch-Mode": "no-cors",
-    "Cache-Control": "max-age=0",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.9",
-    "Accept": "application/json, text/plain, */*",
-    "Referer":"https://www.bilibili.com",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-}
-
 if __name__ == '__main__':
     bv = str(input('视频bv号：'))
     qn = int(input('视频清晰度(参考readme文档)'))
-    link = api.GetVedioLink(bv,qn)[0]
+    link = api.GetVedioLink(bv, qn)[0]
     wget = 'wget --referer "http://www.bilibili.com" "' + link + '"  -O vedio.fly'
     os.system(wget)
