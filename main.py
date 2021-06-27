44626493351bd5a46bce922887a766c796fe99d2
ancba <ancba@outlook.com>
2021-06-27 23:53:36 +0800

    UPDATE

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-import api
+import urllib3
+import json
 import os
 
-if __name__ == '__main__':
+http = urllib3.PoolManager()
+
+def GetVedioInfo (bv):
+    fields = {'bvid':bv}
+    request = http.request(method='GET',url='http://api.bilibili.com/x/web-interface/view',fields=fields)
+    return json.loads(request.data.decode("utf-8"))
+
+def GetVedioLink (bv,qn=0):
+    '''
+    获取视频的直链
+    '''
+
+    info = GetVedioInfo(bv)
+    cid = info['data']['cid']
+    if qn == 0:
+        fields = {'bvid':bv,'cid':cid}
+    else:
+        fields = {'bvid':bv,'cid':cid,'qn':qn}
+    request = http.request(method='GET',url='http://api.bilibili.com/x/player/playurl',fields=fields)
+    back = json.loads(request.data.decode("utf=8"))
+    url = back['data']['durl'][0]['url']
+    backup_url = back['data']['durl'][0]['backup_url'][0]
+    return url,backup_url
+
+if __name__ == "__main__":
     bv = str(input('视频bv号：'))
     qn = int(input('视频清晰度(参考readme文档)'))
-    link = api.GetVedioLink(bv, qn)[0]
+    link = GetVedioLink(bv, qn)[0]
     cookie = str(input("cookie文件存放地址(可跳过)"))
     if qn == 16 or qn == 32:
         wget = 'wget --referer "http://www.bilibili.com" "' + link + '" -O vedio.fly'
